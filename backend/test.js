7ad34523d261cd8a175ce7f7bae44e581a6ec909
omzkiii <geogeomarsantos@gmail.com>
2025-08-27 01:27:25 +0800

    init database and backend connection

--- backend/test.js
+++ backend/test.js
@@ -1,39 +1,35 @@
 // test.js (CJS)
-const { ogr2ogr } = require('ogr2ogr');
+const { ogr2ogr } = require("ogr2ogr");
 
-const { promisify } = require('util');
-const { exec } = require('child_process');
+const { promisify } = require("util");
+const { exec } = require("child_process");
 const execAsync = promisify(exec);
 
-
 async function kmzToGeoJSON(inputPath, layer) {
-    const { data } = await ogr2ogr(inputPath, { 
-        format: 'GeoJSON',
-        options: ['-sql', `SELECT * FROM "${layer}"`] });
+  const { data } = await ogr2ogr(inputPath, {
+    format: "GeoJSON",
+    options: ["-sql", `SELECT * FROM "${layer}"`],
+  });
 
   return data;
 }
 
-
 async function listAllLayers(inputPath) {
+  const { stdout } = await execAsync(`ogrinfo -ro -q -so "${inputPath}"`, {
+    maxBuffer: 1024 * 1024 * 16,
+  });
+  console.log(stdout);
 
-    const { stdout } = await execAsync(`ogrinfo -ro -q -so "${inputPath}"`, {
-        maxBuffer: 1024 * 1024 * 16,
-    });
-    console.log(stdout);
+  const layers = [];
 
-    const layers = [];
+  //String Extract
+  for (const line of stdout.split(/\r?\n/)) {
+    const m = line.match(/^\s*\d+:\s+(.+?)(?:\s+\(|$)/);
+    if (m) layers.push(m[1]);
+  }
 
-    //String Extract
-    for (const line of stdout.split(/\r?\n/)) {
-        const m = line.match(/^\s*\d+:\s+(.+?)(?:\s+\(|$)/);
-        if (m) layers.push(m[1]);
-    }
-
-    console.log(layers);;
-    return layers;
-
+  console.log(layers);
+  return layers;
 }
-
 
 module.exports = { kmzToGeoJSON, listAllLayers };
