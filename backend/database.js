5a3263913b4dd1b0dc82ba6df845c928b234375e
omzkiii <geogeomarsantos@gmail.com>
2025-08-27 03:28:55 +0800

    backend: add migration file, create tables

--- backend/database.js
+++ backend/database.js
@@ -6,21 +6,6 @@ const pool = new Pool();
 async function dbInit() {
   const client = await pool.connect();
 
-  // Create a table
-  await client.query(`
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name TEXT
-    )
-  `);
-
-  // Insert
-  await client.query("INSERT INTO users (name) VALUES ($1)", ["Alice"]);
-
-  // Select
-  const res = await client.query("SELECT * FROM users");
-  console.log(res.rows);
-
   client.release();
 }
 
