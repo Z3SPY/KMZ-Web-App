103e8b7fed491317bbee03d797f0d285610eb8bb
omzkiii <geogeomarsantos@gmail.com>
2025-08-29 00:39:34 +0800

    backend: add get query by id to files, layers, and features

---
+++ backend/controllers/layers.js
@@ -0,0 +1,21 @@
+import { PGISPool } from "../database.js";
+
+export async function getLayers(fileId) {
+  const client = await PGISPool.connect();
+  try {
+    await client.query("BEGIN");
+
+    const layerList = await client.query(
+      `SELECT id, name FROM "LAYERS" WHERE kmz_id = $1`,
+      [fileId],
+    );
+
+    await client.query("COMMIT");
+    return layerList.rows;
+  } catch (err) {
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+  }
+}
--- backend/routes/layers.js
+++ backend/routes/layers.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { getLayers } from "../controllers/layers.js";
 
 export const layerRoutes = Router();
 
-layerRoutes.get("/", (req, res) => {});
+layerRoutes.get("/:fileId", async (req, res) => {
+  const { fileId } = req.params;
+  console.log(fileId);
+  const layers = await getLayers(fileId);
+  res.json(layers);
+});
